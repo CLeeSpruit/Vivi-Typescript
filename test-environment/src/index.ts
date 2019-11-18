da569de66cf0f4062972d45b2b5aad29f7950f15
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2019-11-17 18:23:26 -0600

    test(integration): added jest to integration environemtn

--- test-environment/src/index.ts
+++ test-environment/src/index.ts
@@ -3,11 +3,15 @@ import { ContainerComponent } from './container/container.component';
 import { ChildComponent } from './child/child.component';
 import { ChildWithParamsComponent } from './child-with-params/child-with-params.component';
 
-const vivi = new ModuleFactory({
-    componentConstructors: [
-        { constructor: ContainerComponent },
-        { constructor: ChildComponent },
-        { constructor: ChildWithParamsComponent }
-    ],
-    rootComponent: ContainerComponent
-});
+export const createModule = () => {
+    return new ModuleFactory({
+        componentConstructors: [
+            { constructor: ContainerComponent },
+            { constructor: ChildComponent },
+            { constructor: ChildWithParamsComponent }
+        ],
+        rootComponent: ContainerComponent
+    });
+}
+
+const vivi = createModule();
